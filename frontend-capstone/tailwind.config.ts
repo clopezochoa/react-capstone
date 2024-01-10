ed59a473765d575c4d53ccb4d80df1c525100fe0
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-01-10 13:45:54 +0100

    daisyUI and configurations

--- frontend-capstone/tailwind.config.ts
+++ frontend-capstone/tailwind.config.ts
@@ -7,17 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-krub)'],
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        clear: '#DEDEDE',
+        main: '#57CBEF',
+        focus: '#DA22EF',
+        dark: '#384549'
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
 export default config
