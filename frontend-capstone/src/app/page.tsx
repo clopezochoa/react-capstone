4adbdd57078f3ee324f25311babe4c36b8a0c8c4
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-01-11 20:28:48 +0100

    landing page

--- frontend-capstone/src/app/page.tsx
+++ frontend-capstone/src/app/page.tsx
@@ -1,7 +1,27 @@
+import '../styles/landing.css'
+import fonts from './lib/fonts'
+
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen bg-black"></div>
+      <div className="landing bg-white">
+        <div className={fonts[1].className}>
+          <h1>
+            Your Health
+          </h1>
+          <h2>
+            Our Responsibility
+          </h2>
+        </div>
+        <div className="message-container">
+          <p className={fonts[2].className}>
+          “Our health is more than just the absence of disease; it&apos;s a state of physical, mental, and emotional well-being. Embracing self-awareness, or the ability to understand and accept one&apos;s own thoughts, feelings, and behaviors, is crucial for maintaining and enhancing our overall health.” 
+          </p>
+          <div className={fonts[0].className}>
+            <button className="btn start-button text-white bg-main mt-12">Get Started</button>
+          </div>
+        </div>
+      </div>
   </>
   )
 }
