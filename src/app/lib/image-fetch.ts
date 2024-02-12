27d8dbd8986fa29d969b80012b7e8d50c2261265
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-12 12:08:49 +0100

    clean unused var

--- src/app/lib/image-fetch.ts
+++ src/app/lib/image-fetch.ts
@@ -1,5 +1,4 @@
 export function fetchImage(filename: string, filetype: string, format: string): string {
   const bucket = "https://storage.googleapis.com/carlos-private-cdn/capstone-project/";
-  const images = document.getElementsByTagName('img');
   return `${bucket}${filetype}/${format}/${filename}.${format}`;
 }
