43ce7fb807d16f7b35e53bcbdff41bc1e6f2af2a
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-12 09:55:20 +0100

    fix bad document reference (server)

--- src/app/ui/client-application.tsx
+++ src/app/ui/client-application.tsx
@@ -2,17 +2,17 @@
 
 import { useEffect } from "react";
 
-const body = document.querySelector('body');
-const resizeWindow = () => {
-  if (body) {
-    body.style.height = `${window.innerHeight}px`;
-    const footer = body.querySelector('footer')!;
-    footer.style.visibility = "visible"; 
-  }
-};
 
 export default function ClientApplication({children}: any) {
   useEffect(() => {
+    const body = document.querySelector('body');
+    const resizeWindow = () => {
+      if (body) {
+        body.style.height = `${window.innerHeight}px`;
+        const footer = body.querySelector('footer')!;
+        footer.style.visibility = "visible"; 
+      }
+    };
     resizeWindow();
     window.removeEventListener("resize", resizeWindow)
     window.addEventListener('resize', resizeWindow);
